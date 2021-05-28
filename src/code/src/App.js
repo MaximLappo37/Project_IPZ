2bb3fe5d73c19a953a59c89032a79621aee4a8cb
Lazarev286 <66672363+Lazarev286@users.noreply.github.com>
2021-05-28 15:49:59 +0300

    Add files via upload

---
+++ src/code/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Header from './Components/Header';
+const App = () => {
+    return (
+        <Header/>
+    );
+};
+
+export default App;
